982cee6d55c8f1fef41bf43eff745b1a068ccf44
Breedlove13 <b.breedlove@outlook.com>
2021-04-06 19:26:28 -0700

    putting current day weather on the page

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,10 @@
 
 const fromEl = document.querySelector('#user-form')
 const searchInputEl = document.querySelector('#citySearch')
+const  resultContainer = document.querySelector('#weatherResults')
+const  searchTermEl  = document.querySelector('#city-search')
 
+let today = moment();
 //added submit button function to ccapture input
 const submitButton  =function (e){
     e.preventDefault()
@@ -13,10 +16,10 @@ const submitButton  =function (e){
         weatherByCity(cityName);
         searchInputEl.value= " ";
     }else {
-        alert('Please enter City Name');
+        alert('Please enter a City Name');
     }
 
-    console.log(e)
+    //console.log(e)
 }
 
 fromEl.addEventListener("submit", submitButton)
@@ -24,17 +27,57 @@ fromEl.addEventListener("submit", submitButton)
 
 const weatherByCity = function (searchInput) {
 
-let weatherApi =  "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput +  "&APPID=d1a98a17848dd5228d6e0d33fd081195"
+let weatherApi =  "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&APPID=d1a98a17848dd5228d6e0d33fd081195"
 
 fetch(weatherApi).then(function (response) {
-    console.log(response);
+    //console.log(response);
     response.json().then(function (data) {
-      console.log(data);
+      displayWeather(data, searchInput)
     });
   });
 };
 
-weatherByCity("Phoenix");
+var displayWeather = function (data, searchInput) {
+ console.log(data);
+  //console.log(searchInput);
+  resultContainer.textContent = " ";
+  searchTermEl.textContent = searchInput
+
+let weather = document.createElement('h1')
+
+const  icon = data.weather[0].icon;
+
+weather.innerHTML = `<div class= "dashboard" > 
+                        <h1> ${data.name} (${today.format("MMM Do YY")})  </h1>
+                        <p> General Description: ${data.weather[0].description}</p>
+                        <p> Tempature: ${data.main.temp} </p>
+                        <p> Wind Speed:  ${data.wind.speed} MPH</p>
+                        <p> Humidity:  ${data.main.humidity}% </p>
+                        <img src="icons/${icon}.png">
+                    </div>`;
+ resultContainer.appendChild(weather) 
+
+};
+
+
+//weatherByCity("Phoenix");
+
+
+
+//* we want to 
+//*getcity name, 
+//*the date, 
+//*an icon representation of weather conditions, 
+//*the temperature, 
+//*the humidity, 
+//*the wind speed, 
+//*the UV index
+
+
+//* 5-day forecast
+
+//* need to save past searches to local storage
+
 
 
 
